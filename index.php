291fefcec231303bc421ac176ef48e6b59663d4c
desarrollo-lucho <luisantoniogomez0706@gmail.com>
2025-09-24 19:20:02 -0500

    Actualizacion Index php 6 Septiembre

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once "views/layout/header.php";
+require_once "views/layout/sidebar.php";
+//El controlador Frontal se encuentra aqui
+require_once "views/producto/destacados.php";
+require_once "views/layout/footer.php";
